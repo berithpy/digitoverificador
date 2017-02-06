317e7c882f10628c10a91c898240e15b9aca58f1
Diego Díaz <libre.free.20@gmail.com>
2017-02-05 22:29:25 -0300

    Indent fixes

--- python/digito_verificador.py
+++ python/digito_verificador.py
@@ -1,6 +1,6 @@
 def digito_verificador(ruc,baseMax=11)
-	result = 0
-	for index,ruc_index in enumerate(range(len(ruc)-1,-1,-1)):
-	    result += int(ruc[ruc_index])*(index+2)
-		r = result % baseMax
-	verificador= baseMax - r if r > 1 else 0
+        result = 0
+        for index,ruc_index in enumerate(range(len(ruc)-1,-1,-1)):
+                result += int(ruc[ruc_index])*(index+2)
+                r = result % baseMax
+        verificador= baseMax - r if r > 1 else 0
