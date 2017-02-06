a9cb57a046d80ac43469211ebde3d5987065eb36
Diego Díaz <libre.free.20@gmail.com>
2017-02-05 22:32:56 -0300

    🕰️

--- python/digito_verificador.py
+++ python/digito_verificador.py
@@ -3,5 +3,5 @@ def digito_verificador(ruc,base_max=11)
         for index,ruc_index in enumerate(range(len(ruc)-1,-1,-1)):
                 result += int(ruc[ruc_index])*(index+2)
                 r = result % base_max
-        verificador= baseMax - r if r > 1 else 0
+        verificador= base_max - r if r > 1 else 0
         return verificador
