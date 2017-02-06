9cb0b0277bb055aa8ef7632a5a933f51856a5b48
Diego Díaz <berith.py@gmail.com>
2017-02-05 23:11:16 -0300

    Agregado script js

---
+++ javascript/digitoVerificador.js
@@ -0,0 +1,12 @@
+digitoVerificador = function (ruc, baseMax = 11) {
+    ruc = typeof ruc === 'string' ? ruc : ruc.toString()
+    var resultado = 0
+    for (var rucIndex = ruc.length - 1; rucIndex >= 0; rucIndex--) {
+        var index = 1;
+        resultado += parseInt(ruc[rucIndex]) * (index + 2);
+        var r = resultado % baseMax
+        index++;
+    };
+    var verificador = r > 1 ? baseMax - r : 0
+    return verificador
+}
